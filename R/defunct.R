68006c80a45781b1ad662cf4ab9f7bce528a45c3
hpages@fhcrc.org <hpages@fhcrc.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-10-17 19:00:47 +0000

    Creating branch for BioC 3.4 release
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_4/madman/Rpacks/EBImage@122711 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/defunct.R
@@ -0,0 +1,29 @@
+# nocov start
+erodeGreyScale = function() {
+  .Defunct("erode")
+}
+
+dilateGreyScale = function() {
+  .Defunct("dilate")
+}
+
+openingGreyScale = function() {
+  .Defunct("opening")
+}
+
+closingGreyScale = function() {
+  .Defunct("closing")
+}
+
+whiteTopHatGreyScale = function() {
+  .Defunct("whiteTopHat")
+}
+
+blackTopHatGreyScale = function() {
+  .Defunct("blackTopHat")
+}
+
+selfcomplementaryTopHatGreyScale = function() {
+  .Defunct("selfComplementaryTopHat")
+}
+# nocov end
---
+++ src/tools.h
@@ -0,0 +1,36 @@
+#ifndef EBIMAGE_TOOLS_H
+#define EBIMAGE_TOOLS_H
+
+#include <R.h>
+#include <Rdefines.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MODE_GRAYSCALE  0
+#define MODE_COLOR      2
+
+#define COLOR_MODE(x) ( R_has_slot(x, mkString("colormode")) ? INTEGER(GET_SLOT(x, mkString("colormode")))[0] : MODE_GRAYSCALE )
+
+#define INDEX_FROM_XY(x, y, xsize) ((x) + (y) * (xsize))
+#define POINT_FROM_INDEX(pt, index, xsize) pt.x = index % xsize; pt.y = index / xsize;
+
+#define DISTANCE_XY(pt1, pt2) sqrt( (long double) ( (pt1.x - pt2.x) * (pt1.x - pt2.x) + (pt1.y - pt2.y) * (pt1.y - pt2.y) ) )
+
+typedef struct {
+    int x, y;
+} PointXY;
+  
+typedef double numeric;
+
+int validImage (SEXP x,int test);
+int getNumberOfFrames (SEXP x, int type);
+int getNumberOfChannels (SEXP x);
+void getColorStrides(SEXP x,int index,int *redstride,int *greenstride,int *bluestride);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
